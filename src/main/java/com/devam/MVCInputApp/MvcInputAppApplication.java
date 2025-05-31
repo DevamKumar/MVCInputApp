39a28e318e6319009d0d4cbb0083d1593f60af3d
Devam Kumar <email2devam@gmail.com>
2025-05-31 16:11:03 +0530

    Initial commit

---
+++ src/main/java/com/devam/MVCInputApp/MvcInputAppApplication.java
@@ -0,0 +1,13 @@
+package com.devam.MVCInputApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcInputAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvcInputAppApplication.class, args);
+	}
+
+}
