9b90301d5bdc5c6f4a2a8d02bdb438114a9543e8
Devam Kumar <email2devam@gmail.com>
2025-06-05 12:08:26 +0530

    changed the data type of the primary key of table.

--- src/main/java/com/devam/MVCInputApp/Repository/MVCAppRepo.java
+++ src/main/java/com/devam/MVCInputApp/Repository/MVCAppRepo.java
@@ -3,5 +3,5 @@ package com.devam.MVCInputApp.Repository;
 import com.devam.MVCInputApp.Model.MVCApp;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
-public interface MVCAppRepo extends MongoRepository<MVCApp, Integer> {
+public interface MVCAppRepo extends MongoRepository<MVCApp, String> {
 }
